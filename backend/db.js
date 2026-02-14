0daf36c66bcdcb3f13a30fa06691e4e00d437cdc
higoe3321 <higoe3321@gmail.com>
2026-02-13 23:03:28 -0300

    commit banco produção

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,16 @@
 const mysql = require("mysql2");
 
 const conexao = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "ruticad"
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 conexao.connect((err) => {
     if (err) {
-        console.error("erro ao conectar no MySQL:", err);
+        console.error("Erro ao conectar no MySQL:", err);
     } else {
         console.log("MySQL conectado com sucesso!");
     }
