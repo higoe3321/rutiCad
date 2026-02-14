a64fb6f1bd131f77d1335572f0a256280dffe794
higoe3321 <higoe3321@gmail.com>
2026-02-13 23:34:47 -0300

    commit frontend servindo no back

--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,29 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
-// middlewares
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Servir arquivos estáticos do frontend
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+// Rota principal → abrir login.html
 app.get("/", (req, res) => {
-  res.send("Servidor OK");
+  res.sendFile(path.join(__dirname, "../frontend/login.html"));
 });
 
-const path = require("path");
-
-app.use("/frontend", express.static(path.join(__dirname, "../frontend")));
-
-
-// rotas
+// Rotas da API
 const routes = require("./routes.js");
 app.use("/api", routes);
 
-// servidor
-const PORT = 3000;
+// Porta correta para produção
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
