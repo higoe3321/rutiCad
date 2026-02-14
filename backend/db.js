3b2835b1038b6139a95eec01c622e6031990e1f8
higoe3321 <higoe3321@gmail.com>
2026-02-13 23:46:01 -0300

    commit frontend servindo no back 02

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,13 @@
 const mysql = require("mysql2");
 
-const conexao = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
-});
+const conexao = mysql.createConnection(process.env.MYSQL_URL);
 
 conexao.connect((err) => {
-    if (err) {
-        console.error("Erro ao conectar no MySQL:", err);
-    } else {
-        console.log("MySQL conectado com sucesso!");
-    }
+  if (err) {
+    console.error("Erro ao conectar no MySQL:", err);
+  } else {
+    console.log("MySQL conectado com sucesso!");
+  }
 });
 
 module.exports = conexao;
