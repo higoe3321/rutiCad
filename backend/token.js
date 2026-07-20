4144cd6fb8be9e05561372fd2cb1b48786d6933c
higoe3321 <87220405+higoe3321@users.noreply.github.com>
2026-07-20 20:16:53 -0300

    Update token.js

--- backend/token.js
+++ backend/token.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
 const { google } = require("googleapis");
 const CREDENTIALS = require("./credentials.json");
 
@@ -8,14 +7,13 @@ const oAuth2Client = new google.auth.OAuth2(
   "http://localhost:3000/oauth2callback"
 );
 
-oAuth2Client.getToken(
-  "4/0AfrIepCZAx4DE-6MHEkmuiB06OINMz6111DbG-SbhwbgY3HIGOtkaPUcWhqU6-GGc1_jfg&scope",
-  (err, token) => {
-    if (err) {
-      console.error("Erro ao gerar token:", err);
-      return;
-    }
-    fs.writeFileSync("token.json", JSON.stringify(token));
-    console.log("Token gerado!");
-  }
-);
+const authUrl = oAuth2Client.generateAuthUrl({
+  access_type: "offline",
+  scope: [
+    "https://www.googleapis.com/auth/gmail.send"
+  ],
+  prompt: "consent",
+});
+
+console.log("Abra essa URL:");
+console.log(authUrl);
